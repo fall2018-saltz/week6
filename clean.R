418a60b33fd1f7a789dd231210c8b7e98ebb72e6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:45:09 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,7 +3,9 @@ summary(raw_data)
 raw_data
 #delete 53rd row
 readStates<-function(new_raw_data)
+{
 new_raw_data<- raw_data[-53,]
 str(new_raw_data)
 new_raw_data[1:10,]
+}
 #remove first four columns
