ff88e75e08d594b841a61b95a6dc717570748bec
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 15:38:08 -0400

    Saving merged_data.R

--- merged_data.R
+++ merged_data.R
@@ -2,3 +2,12 @@
 #Using merged data create a histogram
 #load ggplot
 library(ggplot2)
+# Basic histogram
+ggplot(merge, aes(x=weight)) + geom_histogram()
+# Change the width of bins
+ggplot(merge, aes(x=weight)) + 
+  geom_histogram(binwidth=1)
+# Change colors
+p<-ggplot(merge, aes(x=weight)) + 
+  geom_histogram(color="black", fill="white")
+p
