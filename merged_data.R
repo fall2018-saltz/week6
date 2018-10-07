5fbda146e2d5682b7582f3c69e9c636615e093f7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 12:31:22 -0400

    Saving merged_data.R

--- merged_data.R
+++ merged_data.R
@@ -1,10 +1,10 @@
 
 #Using merged data create a histogram
 #load ggplot
-dim(merge)
+dim(merge2)
 library(ggplot2)
 # Basic histogram the width of bins
-pop_hist<-ggplot(merge, aes(x=population)) + geom_histogram(binwidth=30, color="black", fill="white") 
+pop_hist<-ggplot(merge2, aes(x=population)) + geom_histogram(binwidth=30, color="black", fill="white") 
 # Change colors
 
 
